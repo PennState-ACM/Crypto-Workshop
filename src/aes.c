bf32d039423d9370badaac17009b509dbc53e259
shane tully <esc201@gmail.com>
2012-12-02 18:10:11 -0500

    renamed some variables

--- src/aes.c
+++ src/aes.c
@@ -7,7 +7,7 @@ int aes_gen_key(unsigned char *key, unsigned char *iv) {
 
 
 int aes_encrypt(EVP_CIPHER_CTX* context, const unsigned char *key, const unsigned char *iv, const unsigned char *msg, size_t msg_len, unsigned char **enc_msg) {
-    size_t block_len  = 0;
+    size_t block_len   = 0;
     size_t enc_msg_len = 0;
 
     *enc_msg = (unsigned char*)malloc(msg_len + AES_BLOCK_SIZE);
@@ -24,20 +24,20 @@ int aes_encrypt(EVP_CIPHER_CTX* context, const unsigned char *key, const unsigne
 
 
 int aes_decrypt(EVP_CIPHER_CTX* context, const unsigned char *key, const unsigned char *iv, unsigned char *enc_msg, size_t enc_msg_len, char **dec_msg) {
-    size_t dec_len   = 0;
-    size_t block_len = 0;
+    size_t block_len   = 0;
+    size_t dec_msg_len = 0;
 
     *dec_msg = (char*)malloc(enc_msg_len);
 
     EVP_DecryptInit(context, EVP_aes_256_cbc(), key, iv);
 
     EVP_DecryptUpdate(context, (unsigned char*)*dec_msg, (int*)&block_len, enc_msg, (int)enc_msg_len);
-    dec_len += block_len;
+    dec_msg_len += block_len;
 
-    EVP_DecryptFinal_ex(context, (unsigned char*)*dec_msg + dec_len, (int*)&block_len);
-    dec_len += block_len;
+    EVP_DecryptFinal_ex(context, (unsigned char*)*dec_msg + dec_msg_len, (int*)&block_len);
+    dec_msg_len += block_len;
 
-    (*dec_msg)[dec_len] = '\0';
+    (*dec_msg)[dec_msg_len] = '\0';
 
     return enc_msg_len;
 }
